8e459ff6450d48d9e2554e07a667ab850bf878f0
waqarkhan9 <waqarkhan9@yahoo.com>
2024-05-16 00:03:55 +0100

    changes to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+import express from "express";
+import mysql from "mysql";
+
+
+
+const app = express();
+
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password: "root", 
+    database: "test"
+
+})
+
+app.get("/", (req, res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/books",(req, res)=>{
+    const q = "SELECT * FROM BOOKS";
+    db.query(q,(err, data)=>{
+        if(err) return res.json(err)
+            return res.json(data);
+    })
+})
+
+
+app.listen(8800, ()=>{
+console.log("connected to backend");
+})
